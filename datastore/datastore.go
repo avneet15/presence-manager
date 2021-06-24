d291c9bb22541df3fd8838bc86cb7003d84f69b3
avneet15 <aoberoi@lyft.com>
2021-06-23 20:35:47 -0700

    Setup a datastore

--- datastore/datastore.go
+++ datastore/datastore.go
@@ -2,9 +2,10 @@ package datastore
 
 import (
 	"github.com/go-redis/redis"
+	"time"
 )
 type store struct {
-	client redis.client
+	client *redis.Client
 }
 
 func New() *store {
@@ -17,10 +18,22 @@ func New() *store {
 	return &store{client: redisClient}
 }
 
-func (s *store) isEntryPresent(key string) bool {
+func (s *store) IsEntryPresent(key string) bool {
 	_, err := s.client.Get(key).Result()
 	if err != nil {
 		return false
 	}
 	return true
+}
+
+func (s *store) UpsertEntry(key string, ttl_in_seconds int) {
+	if s.IsEntryPresent(key) {
+		pipeline := s.client.Pipeline()
+		pipeline.Get(key)
+		pipeline.Expire(key, time.Duration(ttl_in_seconds)*time.Second)
+		pipeline.Exec()
+	} else {
+		s.client.Set(key, "", time.Duration(ttl_in_seconds)*time.Second)
+	}
+
 }
--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"presence-manager/datastore"
+)
 
 func main(){
 	fmt.Println("hello")
+	ds := datastore.New()
+	//ds.UpsertEntry("A", 5)
+	//
+	//time.Sleep(time.Duration(3)*time.Second)
+	//ds.UpsertEntry("A", 30)
+	//time.Sleep(time.Duration(25)*time.Second)
+	res := ds.IsEntryPresent("A")
+	fmt.Println(res)
 }
