a11e0e92afba8f82cbe5148783c8a9d9bc75f684
avneet15 <aoberoi@lyft.com>
2021-06-23 21:33:01 -0700

    Added http endpoints

--- main.go
+++ main.go
@@ -2,17 +2,30 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"presence-manager/datastore"
 )
 
+var conn = datastore.New()
+
+func processEntryHandler(w http.ResponseWriter, r *http.Request) {
+	entryId := r.URL.Path[15:]
+	conn.UpsertEntry(entryId, 15)
+	fmt.Fprintf(w, "%s", entryId)
+}
+
+func getEntryStateHandler(w http.ResponseWriter, r *http.Request) {
+	entryId := r.URL.Path[17:]
+	isAlive := conn.IsEntryPresent(entryId)
+	fmt.Fprintf(w, "%t", isAlive)
+}
+
 func main(){
-	fmt.Println("hello")
-	ds := datastore.New()
-	//ds.UpsertEntry("A", 5)
-	//
-	//time.Sleep(time.Duration(3)*time.Second)
-	//ds.UpsertEntry("A", 30)
-	//time.Sleep(time.Duration(25)*time.Second)
-	res := ds.IsEntryPresent("A")
-	fmt.Println(res)
+	fmt.Println("Starting service..")
+
+	http.HandleFunc("/process-entry/", processEntryHandler)
+	http.HandleFunc("/get-entry-state/", getEntryStateHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
 }
